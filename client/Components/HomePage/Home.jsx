b250c541894302cb3eaecce5451b16fe268ecbe7
arapKering Kirwa <kkirwa230@gmail.com>
2022-11-05 11:36:26 +0300

    restructured the home page

--- client/Components/HomePage/Home.jsx
+++ client/Components/HomePage/Home.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import Image from "next/image";
 import styles from "../HomePage/Home.module.css";
 import image from "../../public/block.svg";
+import { BsArrowRightSquare } from "react-icons/bs";
+import { useRouter } from "next/router";
 
 export const HomePage = () => {
+  const router = useRouter();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    router.push("/shops");
+  };
   return (
     <div className={`${styles.homeComponent}`}>
       <div className={`${styles.blockImages}`}>
@@ -37,8 +45,12 @@ export const HomePage = () => {
         </p>
       </div>
 
-      <button className={`${styles.buttonHome} text-center`}>
-        Get Started
+      <button
+        className={`${styles.buttonHome}  text-center`}
+        type="button"
+        onClick={handleClick}
+      >
+        Get Started <BsArrowRightSquare className="ml-2"></BsArrowRightSquare>
       </button>
     </div>
   );
