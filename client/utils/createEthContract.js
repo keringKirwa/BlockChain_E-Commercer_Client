d94e876cd4c38267a72f749c31659471f73954d3
arapKering Kirwa <kkirwa230@gmail.com>
2022-11-20 14:40:56 +0300

    Redefined the CREATEeTHEREUMcONTRACT FUNCTION

--- client/utils/createEthContract.js
+++ client/utils/createEthContract.js
@@ -2,10 +2,14 @@ import { ethers } from "ethers";
 import { contractABI, contractAddress } from "./constants";
 
 export const createEthereumContract = async (ethereum) => {
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  if (!ethereum) {
+  if (!window.ethereum) {
     return alert("please install metamask");
   }
+  const provider =
+    window.ethereum != null
+      ? new ethers.providers.Web3Provider(window.ethereum)
+      : ethers.providers.getDefaultProvider();
+
   const accounts = await provider.send("eth_requestAccounts", []);
 
   const signer = provider.getSigner();
